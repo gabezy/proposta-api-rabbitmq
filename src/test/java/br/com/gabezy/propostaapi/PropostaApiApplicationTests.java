d6c2f3d5467df41150276798442f52df03eb2498
gabezy <gabrielmoreira2705@gmail.com>
2025-04-03 11:07:09 -0300

    initial commit

---
+++ src/test/java/br/com/gabezy/propostaapi/PropostaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabezy.propostaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
