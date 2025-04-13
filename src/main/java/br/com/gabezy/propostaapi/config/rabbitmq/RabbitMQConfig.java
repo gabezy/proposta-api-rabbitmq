7d347f5b32d2a616ea153350f5a5326ab32cd0a5
gabezy <gabrielmoreira2705@gmail.com>
2025-04-13 17:08:38 -0300

    configurando MessageConverter para trabalhar com JSON

--- src/main/java/br/com/gabezy/propostaapi/config/rabbitmq/RabbitMQConfig.java
+++ src/main/java/br/com/gabezy/propostaapi/config/rabbitmq/RabbitMQConfig.java
@@ -3,6 +3,8 @@ package br.com.gabezy.propostaapi.config.rabbitmq;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -57,6 +59,18 @@ public class RabbitMQConfig {
     public Binding bindingPropostaPendenteMsNotificao() {
         return BindingBuilder.bind(createQueuePropostaPendenteMsNotificao())
                 .to(fanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
